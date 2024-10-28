9e62c9eca1e02c67dbdd7c6f190279ea43ffdbff
Jonathan Krall <jonathan.krall@lexicon.se>
2024-10-28 13:59:51 +0100

    Scaffolded and Added database

---
+++ LexiconIMDB/Data/LexiconIMDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LexiconIMDB.Models.Entities;
+
+namespace LexiconIMDB.Data
+{
+    public class LexiconIMDBContext : DbContext
+    {
+        public LexiconIMDBContext (DbContextOptions<LexiconIMDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; } = default!;
+    }
+}
---
+++ LexiconIMDB/Models/Entities/Movie.cs
@@ -0,0 +1,16 @@
+﻿namespace LexiconIMDB.Models.Entities
+{
+    public class Movie
+    {
+       
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public float Rating { get; set; }
+
+
+    }
+}
