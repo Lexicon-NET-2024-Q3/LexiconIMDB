3f1ee499d81a325e0434c93f1655329f90fec39f
Jonathan Krall <jonathan.krall@lexicon.se>
2024-10-29 13:21:06 +0100

    Added ViewComponent

---
+++ LexiconIMDB/ViewComponents/StarViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using LexiconIMDB.Data;
+using LexiconIMDB.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LexiconIMDB.ViewComponents
+{
+
+    //[ViewComponent] //Om vi inte sätter ViewComponent i namnet
+    public class StarViewComponent : ViewComponent
+    {
+        //private readonly LexiconIMDBContext _context;
+
+        //public StarViewComponent(LexiconIMDBContext context)
+        //{
+        //    _context = context; 
+        //}
+
+        public IViewComponentResult Invoke(float rating)
+        {
+            var roundedRating = (int)Math.Round(rating);
+
+            var model = new StarViewModel
+            {
+                Stars = roundedRating,
+                IsHalfStar = roundedRating % 2 == 1
+            };
+
+            return View(model); 
+        }
+    }
+}
